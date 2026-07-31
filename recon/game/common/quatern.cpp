56ec2f5345309dc7942fe8722ec2db992004d861
Vyacheslav <belov.vyacheslav@gmail.com>
2026-07-31 14:51:53 +0500

    Match matrix to quaternion conversion

--- recon/game/common/quatern.cpp
+++ recon/game/common/quatern.cpp
@@ -93,20 +93,8 @@ void Quatern_MatToQuat(matrixtdef *matrix,tQuat *q)
 
 {
   int tr;
-  int iVar1;
-  int i;
-  u_int uVar2;
-  int iVar3;
-  int xz;
-  int yz;
-  int xy;
-  int z;
-  int wz;
-  int yy;
-  int wy;
-  int wx;
-  int xx;
   int s;
+  int i;
   
   if ((((((matrix->m[0] == 0x10000) && (matrix->m[1] == 0)) && (matrix->m[2] == 0)) &&
        ((matrix->m[3] == 0 && (matrix->m[4] == 0x10000)))) &&
@@ -118,79 +106,65 @@ void Quatern_MatToQuat(matrixtdef *matrix,tQuat *q)
     q->z = 0;
     return;
   }
-  iVar3 = matrix->m[4];
-  iVar1 = matrix->m[0] + iVar3 + matrix->m[8];
-  if (iVar1 < 1) {
-    uVar2 = 0;
-    if ((matrix->m[0] < iVar3) && (uVar2 = 1, iVar3 < matrix->m[8])) {
-      uVar2 = 2;
-    }
-    iVar3 = matrix->m[8];
-    iVar1 = matrix->m[0];
-    if (iVar1 < iVar3) {
-      uVar2 = 2;
-    }
-    if (uVar2 == 1) {
-      iVar3 = fixedsqrt((matrix->m[4] - (iVar3 + iVar1)) + 0x10000);
-      q->y = (short)(iVar3 - (iVar3 >> 0x1f) >> 3);
-      iVar1 = 0;
-      if (iVar3 != 0) {
-        iVar1 = fixeddiv(0x8000,iVar3);
-      }
-      iVar3 = fixedmult(matrix->m[2] - matrix->m[6],iVar1);
-      q->w = (short)(iVar3 >> 2);
-      iVar3 = fixedmult(matrix->m[7] + matrix->m[5],iVar1);
-      q->z = (short)(iVar3 >> 2);
-      iVar1 = fixedmult(matrix->m[3] + matrix->m[1],iVar1);
-      q->x = (short)(iVar1 >> 2);
-      return;
-    }
-    if (1 < uVar2) {
-      if (uVar2 != 2) {
-        return;
-      }
-      iVar3 = fixedsqrt((iVar3 - (iVar1 + matrix->m[4])) + 0x10000);
-      q->z = (short)(iVar3 - (iVar3 >> 0x1f) >> 3);
-      iVar1 = 0;
-      if (iVar3 != 0) {
-        iVar1 = fixeddiv(0x8000,iVar3);
-      }
-      iVar3 = fixedmult(matrix->m[3] - matrix->m[1],iVar1);
-      q->w = (short)(iVar3 >> 2);
-      iVar3 = fixedmult(matrix->m[2] + matrix->m[6],iVar1);
-      q->x = (short)(iVar3 >> 2);
-      iVar1 = fixedmult(matrix->m[5] + matrix->m[7],iVar1);
-      q->y = (short)(iVar1 >> 2);
-      return;
-    }
-    if (uVar2 != 0) {
-      return;
-    }
-    iVar3 = fixedsqrt((iVar1 - (matrix->m[4] + iVar3)) + 0x10000);
-    q->x = (short)(iVar3 - (iVar3 >> 0x1f) >> 3);
-    iVar1 = 0;
-    if (iVar3 != 0) {
-      iVar1 = fixeddiv(0x8000,iVar3);
-    }
-    iVar3 = fixedmult(matrix->m[7] - matrix->m[5],iVar1);
-    q->w = (short)(iVar3 >> 2);
-    iVar3 = fixedmult(matrix->m[3] + matrix->m[1],iVar1);
-    q->y = (short)(iVar3 >> 2);
-    iVar3 = matrix->m[2] + matrix->m[6];
+  tr = matrix->m[0] + matrix->m[4] + matrix->m[8];
+  if (tr > 0) {
+    s = fixedsqrt(tr + 0x10000);
+    q->w = (short)((int)(s + ((u_int)s >> 0x1f)) >> 3);
+    s = fixeddiv(0x8000,s);
+    q->x = (short)(fixedmult(matrix->m[7] - matrix->m[5],s) >> 2);
+    q->y = (short)(fixedmult(matrix->m[2] - matrix->m[6],s) >> 2);
+    q->z = (short)(fixedmult(matrix->m[3] - matrix->m[1],s) >> 2);
+    return;
   }
-  else {
-    iVar1 = fixedsqrt(iVar1 + 0x10000);
-    q->w = (short)(iVar1 - (iVar1 >> 0x1f) >> 3);
-    iVar1 = fixeddiv(0x8000,iVar1);
-    iVar3 = fixedmult(matrix->m[7] - matrix->m[5],iVar1);
-    q->x = (short)(iVar3 >> 2);
-    iVar3 = fixedmult(matrix->m[2] - matrix->m[6],iVar1);
-    q->y = (short)(iVar3 >> 2);
-    iVar3 = matrix->m[3] - matrix->m[1];
+
+  i = 0;
+  if (matrix->m[0] < matrix->m[4]) {
+    i = 1;
+    if (matrix->m[4] < matrix->m[8]) {
+      i = 2;
+    }
   }
-  iVar1 = fixedmult(iVar3,iVar1);
-  q->z = (short)(iVar1 >> 2);
-  return;
+  if (matrix->m[8] > matrix->m[0]) {
+    i = 2;
+  }
+
+  switch (i) {
+  case 0:
+    s = fixedsqrt(matrix->m[0] - (matrix->m[4] + matrix->m[8]) + 0x10000);
+    q->x = (short)((int)(s + ((u_int)s >> 0x1f)) >> 3);
+    if (s != 0) {
+      s = fixeddiv(0x8000,s);
+    }
+    q->w = (short)(fixedmult(matrix->m[7] - matrix->m[5],s) >> 2);
+    q->y = (short)(fixedmult(matrix->m[3] + matrix->m[1],s) >> 2);
+    q->z = (short)(fixedmult(matrix->m[2] + matrix->m[6],s) >> 2);
+    return;
+
+  case 1:
+    s = fixedsqrt(matrix->m[4] - (matrix->m[8] + matrix->m[0]) + 0x10000);
+    q->y = (short)((int)(s + ((u_int)s >> 0x1f)) >> 3);
+    if (s != 0) {
+      s = fixeddiv(0x8000,s);
+    }
+    q->w = (short)(fixedmult(matrix->m[2] - matrix->m[6],s) >> 2);
+    q->z = (short)(fixedmult(matrix->m[7] + matrix->m[5],s) >> 2);
+    q->x = (short)(fixedmult(matrix->m[3] + matrix->m[1],s) >> 2);
+    return;
+
+  case 2:
+    s = fixedsqrt(matrix->m[8] - (matrix->m[0] + matrix->m[4]) + 0x10000);
+    q->z = (short)((int)(s + ((u_int)s >> 0x1f)) >> 3);
+    if (s != 0) {
+      s = fixeddiv(0x8000,s);
+    }
+    q->w = (short)(fixedmult(matrix->m[3] - matrix->m[1],s) >> 2);
+    q->x = (short)(fixedmult(matrix->m[2] + matrix->m[6],s) >> 2);
+    q->y = (short)(fixedmult(matrix->m[5] + matrix->m[7],s) >> 2);
+    return;
+
+  default:
+    return;
+  }
 }
 
 /* end of quatern.cpp */

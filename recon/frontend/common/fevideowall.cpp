71a00c2085ffc9c328914bf5e07453e8c1499b35
Vyacheslav <belov.vyacheslav@gmail.com>
2026-08-03 03:12:04 +0500

    Match video wall transition controls

--- recon/frontend/common/fevideowall.cpp
+++ recon/frontend/common/fevideowall.cpp
@@ -243,9 +243,12 @@ void tVideoWall::Draw()
 void tVideoWall::TurnOff()
 
 {
+  extern int ticksA[] __asm__("ticks");
+
   if (this->fTransitionDirection != -1) {
+    int tickCounter = ticksA[0];
     this->fTransitionDirection = -1;
-    this->fTVTicks = ticks;
+    this->fTVTicks = tickCounter;
   }
   return;
 }
@@ -275,9 +278,12 @@ void tVideoWall::TurnOffInstant()
 void tVideoWall::TurnOn()
 
 {
+  extern int ticksA[] __asm__("ticks");
+
   if (this->fTransitionDirection != 1) {
+    int tickCounter = ticksA[0];
     this->fTransitionDirection = 1;
-    this->fTVTicks = ticks;
+    this->fTVTicks = tickCounter;
   }
   return;
 }

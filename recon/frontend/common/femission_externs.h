450a574a05675e09d9f6ee834078f1a42b31fc90
Vyacheslav <belov.vyacheslav@gmail.com>
2026-08-27 14:01:17 +0500

    Restore exact FEMission SYM type surface

--- recon/frontend/common/femission_externs.h
+++ recon/frontend/common/femission_externs.h
@@ -1,11 +1,18 @@
 /* frontend/common/femission_externs.h - reconstructed externs. NOT original. */
 #ifndef _FE_FEMISSION_EXTERNS_H_
 #define _FE_FEMISSION_EXTERNS_H_
-#include "../../nfs4_types.h"
-#include "../../lib/libfns.h"
+#include "femission_types.h"
 
 extern tfrontEnd frontEnd;
 extern char     *Paths_Paths[];
+
+extern "C" {
+void blockmove(...);
+void *loadfileadr(...);
+int purgememadr(...);
+void *reservememadr(...);
+int sprintf(...);
+}
 
 
 #endif
